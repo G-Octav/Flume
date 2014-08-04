f58ccf4a1abadd39b1a5fffb9c42622583d9a36e
Gensthaler Octavian <octav.gensthaler@gmail.com>
2014-08-03 21:55:52 -0700

    Restructure some buttons

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -11,37 +11,48 @@
 	: window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32), "Flume Tiled Map Editor")
 	{
 		auto tools = sfg::Window::Create();
-		auto layers = sfg::Window::Create();
 		auto box = sfg::Box::Create(sfg::Box::Orientation::VERTICAL, 5.0f);
 		auto layersBox = sfg::Box::Create(sfg::Box::Orientation::VERTICAL, 5.0f);
 		auto button_row = sfg::Box::Create(sfg::Box::Orientation::HORIZONTAL, 0.0f);
+		auto button_row2 = sfg::Box::Create(sfg::Box::Orientation::HORIZONTAL, 5.0f);
 		auto button1 = sfg::Button::Create("Save Map");
 		auto button2 = sfg::Button::Create("Load Map");
+		auto button_add_layer = sfg::Button::Create(" + ");
+		auto button_remove_layer = sfg::Button::Create("  -  ");
+		auto button_layer_up = sfg::Button::Create(" ^ ");
+		auto button_layer_down = sfg::Button::Create("  v  ");
+		auto button_layer_delete = sfg::Button::Create("  x  ");
 		auto infoframe = sfg::Frame::Create("info");
 		auto scrolledwindow = sfg::ScrolledWindow::Create();
 
 		scrolledwindow->SetScrollbarPolicy(sfg::ScrolledWindow::HORIZONTAL_NEVER | sfg::ScrolledWindow::VERTICAL_AUTOMATIC);
-		scrolledwindow->SetRequisition(sf::Vector2f(170.f, 100.f));
+		scrolledwindow->SetRequisition(sf::Vector2f(170.f, 200.f));
 		tools->SetTitle("Tool window");
-		layers->SetTitle("Layer List");
 		tools->SetPosition(sf::Vector2f(30,30));
 
 		std::string frameText = wrap("This is the alpha version of Flume. Use the buttons above to create a new layer and save your work", 20);
 		
 		auto infotext = sfg::Label::Create(frameText);
 		infoframe->Add(infotext);
-		layers->SetPosition(sf::Vector2f(tools->GetAbsolutePosition().x, tools->GetAbsolutePosition().y+tools->GetClientRect().height*5));
+
+		scrolledwindow->Pack()
 
 		button_row->Pack(button1, true);
 		button_row->Pack(button2, true);
+		button_row2->Pack(button_add_layer, false);
+		button_row2->Pack(button_remove_layer, false);
+		button_row2->Pack(button_layer_up, false);
+		button_row2->Pack(button_layer_down, false);
+		button_row2->Pack(button_layer_delete, false);
+
 		box->Pack(button_row);
 		box->Pack(infoframe);
-		layersBox->Pack(sfg::Label::Create("Layer list: "), false, true);
-		layersBox->Pack(scrolledwindow, false, true);
+		box->Pack(sfg::Label::Create("Layer list: "), false, true);
+		box->Pack(button_row2);
+		box->Pack(scrolledwindow, false, true);
+
 		tools->Add(box);
-		layers->Add(layersBox);
 		desktop.Add(tools);
-		desktop.Add(layers);
 	}
 
 /* ----------------------------------------------------------------------
