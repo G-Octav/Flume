6bce3ede41acfdb8bb9a022bd5ef5043b3cd9fda
Gensthaler Octavian <octav.gensthaler@gmail.com>
2014-08-08 18:27:48 -0700

    Practically finished the layout

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -1,3 +1,5 @@
+#include <SFML/Graphics.hpp>
+
 #include "Application.hpp"
 #include "Stdafx.hpp"
 
@@ -10,8 +12,10 @@
 	Application::Application() 
 	: window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32), "Flume Tiled Map Editor")
 	{
+		auto box1 = sfg::Box::Create(sfg::Box::Orientation::VERTICAL, 0.0f);
+		auto box2 = sfg::Box::Create(sfg::Box::Orientation::VERTICAL, 0.0f);
 		auto tools = sfg::Window::Create();
-		auto box = sfg::Box::Create(sfg::Box::Orientation::VERTICAL, 0.0f);
+		auto box = sfg::Box::Create(sfg::Box::Orientation::VERTICAL, 10.0f);
 		auto layersBox = sfg::Box::Create(sfg::Box::Orientation::VERTICAL, 5.0f);
 		auto button_row = sfg::Box::Create(sfg::Box::Orientation::HORIZONTAL, 0.0f);
 		auto button_row2 = sfg::Box::Create(sfg::Box::Orientation::HORIZONTAL, 0.0f);
@@ -22,19 +26,31 @@
 		auto button_remove_layer = sfg::Button::Create("  -  ");
 		auto button_layer_up = sfg::Button::Create("up");
 		auto button_layer_down = sfg::Button::Create("down");
-		auto button_layer_delete = sfg::Button::Create("  x  ");
+		auto btn_hide_layer = sfg::Button::Create("hide");
+		auto button_layer_delete = sfg::Button::Create("delete");
 		auto infoframe = sfg::Frame::Create("info");
 		auto scrolledwindow = sfg::ScrolledWindow::Create();
+		auto tilesettab = sfg::Notebook::Create();
+		auto listBox = sfg::Box::Create(sfg::Box::Orientation::VERTICAL);
 
+		tools->SetStyle(tools->GetStyle() ^ sfg::Window::TITLEBAR);
 		scrolledwindow->SetScrollbarPolicy(sfg::ScrolledWindow::HORIZONTAL_NEVER | sfg::ScrolledWindow::VERTICAL_AUTOMATIC);
+		tilesettab->AppendPage(sfg::Label::Create(), sfg::Label::Create("   Tileset   "));
+		tilesettab->AppendPage(sfg::Label::Create(), sfg::Label::Create("   Properties   "));
+		tilesettab->AppendPage(sfg::Label::Create(), sfg::Label::Create("   Directory   "));
+		tilesettab->SetRequisition(sf::Vector2f(170.f, 500.f));
 		scrolledwindow->SetRequisition(sf::Vector2f(170.f, 200.f));
 		tools->SetTitle("Tool window");
-		tools->SetPosition(sf::Vector2f(30,30));
+		tools->SetRequisition(sf::Vector2f(300.f, 0.f));
 
-		std::string frameText = wrap("This is the alpha version of Flume. Use the buttons above to create a new layer and save your work", 20);
+		std::string frameText = "This is the alpha version of Flume. Use the buttons above to create a new layer and save your work. Flume is still a work in progress, so report any bug you can find on the original repository. There is no open file dialogue yet, in order to load a map you'll have to click on the Directory tab and select one.";
 		
 		auto infotext = sfg::Label::Create(frameText);
+		infotext->SetLineWrap(true);
 		infoframe->Add(infotext);
+
+		listBox->Pack(sfg::Label::Create("test"), true, true);
+		scrolledwindow->Add(listBox);
 
 		button_add_layer->SetId("addlayer");
 		button_remove_layer->SetId("removelayer");
@@ -54,14 +70,19 @@
 		button_row2->Pack(button_add_layer, false);
 		button_row2->Pack(button_remove_layer, false);
 		button_row2->Pack(button_layer_up, true);
-		button_row2->Pack(button_layer_down, false);
-		button_row2->Pack(button_layer_delete, false);
+		button_row2->Pack(button_layer_down, true);
+		button_row2->Pack(btn_hide_layer, true);
+		button_row2->Pack(button_layer_delete, true);
 
-		box->Pack(button_row);
-		box->Pack(button3, true);
-		//box->Pack(sfg::Label::Create("Layer list: "), false, true);
-		box->Pack(button_row2);
-		box->Pack(scrolledwindow, false, true);
+		box1->Pack(button_row);
+		box1->Pack(button3, true);
+		box1->Pack(button_row2);
+		box1->Pack(scrolledwindow);
+
+		box2->Pack(tilesettab);
+
+		box->Pack(box1);
+		box->Pack(box2);
 		box->Pack(infoframe);
 
 		tools->Add(box);
